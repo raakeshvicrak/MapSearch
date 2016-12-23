3a6f2a6f942013d0e8e6116bcd95420f76cf6899
Raakesh Premkumar <raakeshpremkumar@Raakeshs-MacBook-Pro.local>
2016-12-22 23:28:18 -0500

    FileNameChange

--- route.py
+++ route.py
@@ -2,37 +2,37 @@
 #Heuristic function is the straight line distance between the current node and the goal node.
 #for our assignment IDS is the fastest algoritm
 #DFS is the most efficient algorithm in terms of memory. 
-import Assignment_two_final_mapSearch_BFS;
-import Assignment_two_final_mapSearch_DFS;
-import Assignment_two_final_mapSearch_Astar;
-import Assignment_two_final_mapSearch_IDS;
+import mapSearch_BFS;
+import mapSearch_DFS;
+import mapSearch_Astar;
+import mapSearch_IDS;
 import sys;
 
 if sys . argv [ 4 ].lower() == "bfs":
-    Assignment_two_final_mapSearch_BFS.input_function(sys . argv [ 1 ], sys . argv [ 2 ], sys . argv [ 3 ]);
+    mapSearch_BFS.input_function(sys . argv [ 1 ], sys . argv [ 2 ], sys . argv [ 3 ]);
 
-    Assignment_two_final_mapSearch_BFS.file_reading();
+    mapSearch_BFS.file_reading();
 
-    Assignment_two_final_mapSearch_BFS.search_algorithm();
+    mapSearch_BFS.search_algorithm();
 
 elif sys . argv [ 4 ].lower() == "dfs":
-    Assignment_two_final_mapSearch_DFS.input_function(sys . argv [ 1 ], sys . argv [ 2 ], sys . argv [ 3 ]);
+    mapSearch_DFS.input_function(sys . argv [ 1 ], sys . argv [ 2 ], sys . argv [ 3 ]);
 
-    Assignment_two_final_mapSearch_DFS.file_reading();
+    mapSearch_DFS.file_reading();
 
-    Assignment_two_final_mapSearch_DFS.search_algorithm();
+    mapSearch_DFS.search_algorithm();
 
 elif sys . argv [ 4 ].lower() == "astar":
-    Assignment_two_final_mapSearch_Astar.input_function(sys . argv [ 1 ], sys . argv [ 2 ], sys . argv [ 3 ]);
+    mapSearch_Astar.input_function(sys . argv [ 1 ], sys . argv [ 2 ], sys . argv [ 3 ]);
 
-    Assignment_two_final_mapSearch_Astar.file_reading();
+    mapSearch_Astar.file_reading();
 
-    Assignment_two_final_mapSearch_Astar.search_algorithm();
+    mapSearch_Astar.search_algorithm();
 
 elif sys . argv [ 4 ].lower() == "ids":
-    Assignment_two_final_mapSearch_IDS.input_function(sys . argv [ 1 ], sys . argv [ 2 ], sys . argv [ 3 ]);
+    mapSearch_IDS.input_function(sys . argv [ 1 ], sys . argv [ 2 ], sys . argv [ 3 ]);
 
-    Assignment_two_final_mapSearch_IDS.file_reading();
+    mapSearch_IDS.file_reading();
 
-    Assignment_two_final_mapSearch_IDS.search_algorithm();
+    mapSearch_IDS.search_algorithm();
     
